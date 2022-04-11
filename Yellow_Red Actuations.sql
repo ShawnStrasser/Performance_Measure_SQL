ecb757319ab50d51203a4d2b7be6d133d0c216ba
Shawn Strasser <shawn.strasser@odot.state.or.us>
2022-04-11 08:36:27 -0700

    added latency 1.5 seconds

--- Yellow_Red Actuations.sql
+++ Yellow_Red Actuations.sql
@@ -1,13 +1,27 @@
--- Actuations on Yellow, First 4 Seconds of Red, and on Red After 4 Seconds
---EventID 8 = Begin Yellow, 9 = End Yellow, 1 = Begin Green (used as end red)
+/* 
+This query returns aggregate yellow and red actuations in binned periods for a single detector-phase pair.
 
+Included Periods: Actuations on Yellow, First 4 Seconds of Red, and on Red After 4 Seconds
+The 4-second period is the default UDOT uses, it differentiates between "minor" and "severe" violations
+
+A UDOT sponsered study by Avenue Consultants found a 1.5 second average latency from a vehicle 
+entering a radar zone to the timestamp of the actuation being recorded by the controller. Therefore,
+the timestamps for aggregations are shifted by 1.5 seconds to account for that latency. Users should
+keep in mind that 1.5 seconds is the average, the actual distribution varies by unkown factors.
+NOTE: the phase events are shifted forward rather than shifting detector events back, because there's less phase events
+
+These hi-res events are used: EventID 8 = Begin Yellow, 9 = End Yellow, 1 = Begin Green (used as proxy for "end red")
+
+*/
+
+--Set User Variables
 DECLARE @TSSU as VARCHAR(5) ='22036';
 DECLARE @Phase as INT= 5;
 DECLARE @MT as INT= 45;
 DECLARE @BinSize AS INT = 15;
-DECLARE @start DATETIME ='2022-01-06';
+DECLARE @start DATETIME ='2022-01-01';
 DECLARE @end DATETIME  = '2022-04-07';
-
+DECLARE @Latency FLOAT = 1.5;
 
 	--Run query!--
 --use TSSU to look up device id
@@ -17,10 +31,10 @@ SELECT GroupableElements.ID
 FROM [MaxView_1.9.0.744].[dbo].[GroupableElements]
 WHERE Right(GroupableElements.Number,5) = @TSSU)
 
---Yellow: phase yellow on and off events, with start and end timestamps
+--#Yellow: phase yellow on and off events, with start and end timestamps
 SELECT
-	Lag(TimeStamp) OVER (ORDER BY TimeStamp) as BeginYellow,
-	TimeStamp as EndYellow,
+	DATEADD(second, @Latency, Lag(TimeStamp) OVER (ORDER BY TimeStamp)) as BeginYellow,
+	DATEADD(second, @Latency, TimeStamp) as EndYellow,
 	Lead(TimeStamp) OVER (ORDER BY TimeStamp) as EndRed,
 	EventID,
 	Lag(EventID) OVER (ORDER BY TimeStamp) as Previous_EventID,
@@ -33,8 +47,6 @@ WHERE
 	Parameter=@Phase and 
 	TimeStamp >= @start and 
 	TimeStamp < @end
-
-select * from #Yellow order by BeginYellow
 
 --Filter to the End Yellow events where the previous event was begin yellow and next event is end red(begin green)
 --Add Severe_Time column 4 seconds after end yellow
@@ -67,9 +79,6 @@ WHERE
 	TimeStamp >= @start and 
 	TimeStamp < @end
 
-
-
-
 --Total: Counts total actuations in each time period. This is used in denominator to calculate percent in final select statement. 
 SELECT
 	TimePeriod,
@@ -82,35 +91,24 @@ Group By TimePeriod
 --Counts the number of actuations during each period
 SELECT
 	TimePeriod,
-	COUNT(TimePeriod) as Yellow
-FROM #Filtered_Yellow, #Actuations
-WHERE
-	ActuationDate=YellowDate and
-	ArrivalTime >= BeginYellow and
-	ArrivalTime < EndYellow
-Group By TimePeriod
-
-
-SELECT
-	TimePeriod,
 	SUM(CASE WHEN ArrivalTime >= BeginYellow and ArrivalTime < EndYellow THEN 1 ELSE 0 END) as Yellow,
 	SUM(CASE WHEN ArrivalTime >= EndYellow and ArrivalTime < Severe_Time THEN 1 ELSE 0 END) as Red_Minor,
-	SUM(CASE WHEN ArrivalTime >= Severe_Time and ArrivalTime < EndRed THEN 1 ELSE 0 END) as Red_Major
+	SUM(CASE WHEN ArrivalTime >= Severe_Time and ArrivalTime < EndRed THEN 1 ELSE 0 END) as Red_Severe
 INTO #Final
 FROM #Filtered_Yellow, #Actuations
 WHERE ActuationDate=YellowDate --Add time period to the where clause???
 GROUP BY TimePeriod
 
-
+--Calculate percentages
 SELECT
 	#Total.TimePeriod, 
 	Yellow,
 	Red_Minor,
-	Red_Major,
+	Red_Severe,
 	TotalActuations,
 	Yellow * 100 / TotalActuations as Percent_Yellow,
 	Red_Minor * 100 / TotalActuations as Percent_Red_Minor,
-	Red_Major * 100 / TotalActuations as Percent_Red_Major
+	Red_Severe * 100 / TotalActuations as Percent_Red_Major
 FROM #Total
 LEFT JOIN #Final ON #Final.TimePeriod=#Total.TimePeriod
 ORDER BY TimePeriod
