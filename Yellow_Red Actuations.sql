c3711bcfae551e96ad57165c43cb61d1148fa764
Shawn Strasser <shawn.strasser@odot.state.or.us>
2022-04-11 15:55:19 -0700

    changed output table to Tidy format

--- Yellow_Red Actuations.sql
+++ Yellow_Red Actuations.sql
@@ -12,6 +12,21 @@ NOTE: the phase events are shifted forward rather than shifting detector events 
 
 These hi-res events are used: EventID 8 = Begin Yellow, 9 = End Yellow, 1 = Begin Green (used as proxy for "end red")
 
+The result of this query outputs a "tidy" style long table with a Feature column, with numeric codes as follows:
+
+	1 = Actuations during Yellow
+	2 = Actuations during First 4 Seconds of Red (minor violation)
+	3 = Actuations After First 4 Seconds of Red (major violation)
+	4 = Average Time of Yellow Actuations in Milliseconds (avg elapsed time from start of yellow to when actuations during yellow occured)
+	5 = Same as 4, but for first 4 seconds of red
+	6 = Same as 4, but for after first 4 seconds of red
+	7 = Standard Deviation of 4 (milliseconds, no result if sample size < 2)
+	8 = Standard Deviation of 5
+	9 = Standard Deviation of 6
+	10 = Total Actuations over all periods, including green time.
+
+The intent is that the DeviceID, TimePeriod and Feature column will be used as indexes, and the Values column is the data.
+The Feature column will be used for filtering/measures in Power BI
 */
 
 --Set User Variables
@@ -67,6 +82,7 @@ DROP TABLE #Yellow
 
 
 --Actuations: all detector on events for the specified detector, with a rounded timestamp called TimePeriod for joining
+--This step takes the longest in the entire query
 SELECT
 	TimeStamp as ArrivalTime,
 	dateadd(minute, datediff(minute,0,TimeStamp)/@BinSize * @BinSize, 0) as TimePeriod,
@@ -80,83 +96,86 @@ WHERE
 	TimeStamp >= @start and 
 	TimeStamp < @end
 
+
+-----FINAL OUTPUT-----
+--No more temp tables, now final data output from different queries will be unioned together---
 --Total: Counts total actuations in each time period. This is used in denominator to calculate percent in final select statement. 
 SELECT
 	TimePeriod,
-	Count(TimePeriod) as TotalActuations
-INTO #Total
+	10 as Feature,
+	Count(TimePeriod) as "Value"
 FROM #Actuations
-Group By TimePeriod
+GROUP BY TimePeriod
 
-
+UNION ALL
 --Yellow2 Actuations Table
 SELECT
 	TimePeriod,
-	COUNT(*) as Yellow,
-	AVG(DATEDIFF(ms, BeginYellow, ArrivalTime)) as Yellow_Avg_Time,
-	CONVERT(int, STDEV(DATEDIFF(ms, BeginYellow, ArrivalTime))) as Yellow_SD
-INTO #Yellow2
-FROM #Filtered_Yellow
-JOIN #Actuations
-	ON ArrivalTime >= BeginYellow 
-	and ArrivalTime < EndYellow
-	and ActuationDate = YellowDate
-GROUP BY TimePeriod
+	Feature,
+	"Value"
+FROM 
+	(SELECT
+		TimePeriod,
+		COUNT(*) as [1],
+		AVG(DATEDIFF(ms, BeginYellow, ArrivalTime)) as [4],
+		CONVERT(int, STDEV(DATEDIFF(ms, BeginYellow, ArrivalTime))) as [7]
+	FROM #Filtered_Yellow
+	JOIN #Actuations
+		ON ArrivalTime >= BeginYellow 
+		and ArrivalTime < EndYellow
+		and ActuationDate = YellowDate
+	GROUP BY TimePeriod
+	) q
+UNPIVOT	
+	("Value" FOR Feature IN
+		([1], [4], [7])) p
 
+UNION ALL
 --Minor Violation Actuations Table
 SELECT
 	TimePeriod,
-	COUNT(*) as Minor,
-	AVG(DATEDIFF(ms, EndYellow, ArrivalTime)) as Minor_Avg_Time,
-	CONVERT(int, STDEV(DATEDIFF(ms, EndYellow, ArrivalTime))) as Minor_SD
-INTO #Minor
-FROM #Filtered_Yellow
-JOIN #Actuations
-	ON ArrivalTime >= EndYellow 
-	and ArrivalTime < Severe_Time
-	and ActuationDate = YellowDate
-GROUP BY TimePeriod
+	Feature,
+	"Value"
+FROM 
+	(SELECT
+		TimePeriod,
+		COUNT(*) as [2],
+		AVG(DATEDIFF(ms, EndYellow, ArrivalTime)) as [5],
+		CONVERT(int, STDEV(DATEDIFF(ms, EndYellow, ArrivalTime))) as [8]
+	FROM #Filtered_Yellow
+	JOIN #Actuations
+		ON ArrivalTime >= EndYellow 
+		and ArrivalTime < Severe_Time
+		and ActuationDate = YellowDate
+	GROUP BY TimePeriod
+	) q
+UNPIVOT	
+	("Value" FOR Feature IN
+		([2], [5], [8])) p
 
+UNION ALL
 --Severe Violation Actuations Table
 SELECT
 	TimePeriod,
-	COUNT(*) as Severe,
-	AVG(DATEDIFF(ms, Severe_Time, ArrivalTime)) as Severe_Avg_Time,
-	CONVERT(int, STDEV(DATEDIFF(ms, Severe_Time, ArrivalTime))) as Severe_SD
-INTO #Severe
-FROM #Filtered_Yellow
-JOIN #Actuations
-	ON ArrivalTime >= Severe_Time 
-	and ArrivalTime < EndRed
-	and ActuationDate = YellowDate
-GROUP BY TimePeriod
+	Feature,
+	"Value"
+FROM 
+	(SELECT
+		TimePeriod,
+		COUNT(*) as [3],
+		AVG(DATEDIFF(ms, Severe_Time, ArrivalTime)) as [6],
+		CONVERT(int, STDEV(DATEDIFF(ms, Severe_Time, ArrivalTime))) as [9]
+	FROM #Filtered_Yellow
+	JOIN #Actuations
+		ON ArrivalTime >= Severe_Time 
+		and ArrivalTime < EndRed
+		and ActuationDate = YellowDate
+	GROUP BY TimePeriod
+	) q
+UNPIVOT	
+	("Value" FOR Feature IN
+		([3], [6], [9])) p
 
---Join #Yellow, #Minor, #Severe, and Calculate percentages
-SELECT
-	#Total.TimePeriod, 
-	Yellow,
-	Yellow_Avg_Time,
-	Yellow_SD,
-	Minor,
-	Minor_Avg_Time,
-	Minor_SD,
-	Severe,
-	Severe_Avg_Time,
-	Severe_SD,
-	TotalActuations,
-	Yellow * 100 / TotalActuations as Percent_Yellow,
-	Minor * 100 / TotalActuations as Percent_Red_Minor,
-	Severe * 100 / TotalActuations as Percent_Red_Major
-FROM #Total
-LEFT JOIN #Yellow2 ON #Yellow2.TimePeriod=#Total.TimePeriod
-LEFT JOIN #Minor ON #Minor.TimePeriod=#Total.TimePeriod
-LEFT JOIN #Severe ON #Severe.TimePeriod=#Total.TimePeriod
-ORDER BY TimePeriod
-	
 
 DROP TABLE #Filtered_Yellow
 DROP TABLE #Actuations
-DROP TABLE #Total
-DROP TABLE #Yellow2
-DROP TABLE #Minor
-DROP TABLE #Severe
